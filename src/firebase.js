32963fc5539363bca974e7a5627cbb4ceb2fc37b
Fehd <fehdwelling@gmail.com>
2018-04-24 17:10:16 +0200

    - first commit chattos app

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,13 @@
-firebase-key: chattos-6b1f4
+import * as firebase from 'firebase';
 
-
-firebase : <script src="https://www.gstatic.com/firebasejs/4.13.0/firebase.js"></script>
-<script>
-  // Initialize Firebase
-  var config = {
+const config = {
     apiKey: "AIzaSyCFQAh9pxOmw85YGysdD4DEUkPlz5byv0A",
     authDomain: "chattos-6b1f4.firebaseapp.com",
     databaseURL: "https://chattos-6b1f4.firebaseio.com",
     projectId: "chattos-6b1f4",
     storageBucket: "chattos-6b1f4.appspot.com",
     messagingSenderId: "359540951963"
-  };
-  firebase.initializeApp(config);
-</script>
+};
+firebase.initializeApp(config);
+
+export default firebase;
